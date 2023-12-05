52bde8328f0d67b02229ccf8415026d3a0a6e989
Pepijn van de Liefvoort <pepijn.van.de.liefvoort@vionfood.com>
2023-12-05 11:01:42 +0100

    day 5 part 1

---
+++ day-5/Advent Of Code day 5/Garden.cs
@@ -0,0 +1,20 @@
+﻿namespace Advent_Of_Code_day_5
+{
+    class Mapping
+    {
+        public long DestinationStart { get; set; }
+        public long SourceStart { get; set; }
+        public long Length { get; set; }
+    }
+
+    class CategoryMap
+    {
+        public List<Mapping> Mappings { get; set; }
+    }
+
+    class Almanac
+    {
+        public List<long> Seeds { get; set; }
+        public Dictionary<string, CategoryMap> Maps { get; set; }
+    }
+}
