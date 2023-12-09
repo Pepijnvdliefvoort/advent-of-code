7d5fc4c025aa53dc1dc6853de47f935cebb3f7a8
Pepijn van de Liefvoort <pepijn.van.de.liefvoort@vionfood.com>
2023-12-09 21:31:33 +0100

    day 9 part 1

--- day-9/Program.cs
+++ day-9/Program.cs
@@ -11,8 +11,8 @@ namespace Advent_of_Code
         {
             string exeDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-            // string relativeFilePath = Path.Combine("input", "input.txt");
-            string relativeFilePath = Path.Combine("input", "test.txt");
+            string relativeFilePath = Path.Combine("input", "input.txt");
+            //string relativeFilePath = Path.Combine("input", "test.txt");
 
             string filePath = Path.Combine(exeDirectory, relativeFilePath);
 
@@ -43,12 +43,68 @@ namespace Advent_of_Code
 
         private static int PartOne(List<string> rows)
         {
-            return 0;
+            int sum = 0;
+            foreach (string row in rows)
+            {
+                List<int> sequence = row.Split(' ').Select(int.Parse).ToList();
+                List<List<int>> report = new List<List<int>>();
+                report.Add(sequence);
+
+                int index = 0;
+                var test = report.Last().GroupBy(num => num).Count();
+                while (report.Last()[0] != 0 || report.Last().GroupBy(num => num).Count() != 1)
+                {
+                    report.Add(GetDifferences(report[index]));
+
+                    index++;
+                }
+
+                int num = CalculateNextNumber(report);
+                sum += num;
+            }
+
+            return sum;
         }
 
         private static int PartTwo(List<string> rows)
         {
             return 0;
+        }
+
+        private static List<int> GetDifferences(List<int> sequence)
+        {
+            List<int> differences = new List<int>();
+
+            int previous = -9999999;
+            foreach (int current in sequence)
+            {
+                if (previous == -9999999)
+                {
+                    previous = current;
+                    continue;
+                }
+
+                int diff = current - previous;
+                previous = current;
+                differences.Add(diff);
+            }
+
+            return differences;
+        }
+
+        private static int CalculateNextNumber(List<List<int>> report)
+        {
+            report.Last().Add(0);
+
+            for (int i = report.Count() - 2; i >= 0; i--)
+            {
+                var down = report[i + 1].Last();
+                var left = report[i].Last();
+
+                report[i].Add(down + left);
+            }
+
+            return report[0].Last();
         }
     }
 }
