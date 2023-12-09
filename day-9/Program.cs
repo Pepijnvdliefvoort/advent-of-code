1b703d3fe14b6e1a7e267029da7c646bfb73664c
Pepijn van de Liefvoort <pepijn.van.de.liefvoort@vionfood.com>
2023-12-09 17:24:03 +0100

    initial commit day 9

---
+++ day-9/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Advent_of_Code
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string exeDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // string relativeFilePath = Path.Combine("input", "input.txt");
+            string relativeFilePath = Path.Combine("input", "test.txt");
+
+            string filePath = Path.Combine(exeDirectory, relativeFilePath);
+
+            try
+            {
+                // Check if the file exists
+                if (File.Exists(filePath))
+                {
+                    var lines = File.ReadLines(relativeFilePath);
+
+                    // Display the content
+                    Console.WriteLine("Part One: " + PartOne(lines!.ToList()));
+                    Console.WriteLine("Part Two: " + PartTwo(lines!.ToList()));
+                }
+                else
+                {
+                    Console.WriteLine("File not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static int PartOne(List<string> rows)
+        {
+            return 0;
+        }
+
+        private static int PartTwo(List<string> rows)
+        {
+            return 0;
+        }
+    }
+}
