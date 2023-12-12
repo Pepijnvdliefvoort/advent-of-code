2ff9c152104fdfa3bb77325ae0c8919c352a9c8e
Pepijn van de Liefvoort <pepijn.van.de.liefvoort@vionfood.com>
2023-12-12 15:37:03 +0100

    changes day 12

---
+++ day-12/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Advent_of_Code
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string exeDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // string relativeFilePath = Path.Combine("input", "input.txt");
+            string relativeFilePath = Path.Combine("input", "test.txt");
+
+            string filePath = Path.Combine(exeDirectory, relativeFilePath);
+
+            try
+            {
+                // Check if the file exists
+                if (File.Exists(filePath))
+                {
+                    var lines = File.ReadLines(relativeFilePath);
+
+                    // Display the content
+                    Console.WriteLine("Part One: " + PartOne(lines!.ToList()));
+                    Console.WriteLine("Part Two: " + PartTwo(lines!.ToList()));
+                }
+                else
+                {
+                    Console.WriteLine("File not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static int PartOne(List<string> rows)
+        {
+            int sum = 0;
+            foreach (string row in rows)
+            {
+                var springs = row.Split(" ")[0];
+                var groups = row.Split(" ")[1].Split(",").Select(int.Parse).ToList();
+
+                sum += GetArrangements(springs, groups);
+            }
+
+            return 0;
+        }
+
+        private static int PartTwo(List<string> rows)
+        {
+            return 0;
+        }
+
+        private static int GetArrangements(string input, List<int> order)
+        {
+            int arrangements = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Equals('.'))
+                    continue;
+
+                // Beginning of string
+                if (input[i].Equals('.') && (input[i + 1].Equals('#') || input[i + 1].Equals('?')))
+                    continue;
+
+                arrangements++;
+            }
+
+            return arrangements;
+        }
+    }
+}
